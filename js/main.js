4350fb2bfa4121932d5c18b3755f19f5f95314a7
Alessandro Lepore <lepore.ale@gmail.com>
2025-10-02 20:20:18 +0200

    Fix open script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
-document.querySelector('[data-link]').addEventListener('click', function() {
+document.querySelector('[data-link]').addEventListener('click', function(event) {
+  event.preventDefault();
   open(atob(this.dataset.link), this.target)
 }, false);
